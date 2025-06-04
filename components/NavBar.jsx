36f7c0bb3514883402db3d536446d0c411ecc914
Lakshan18 <lakshanvlogs3276@gmail.com>
2025-06-04 20:53:12 +0530

    Portfolio
    Continuing navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 const NavBar = () => {
 
     const [active, setActive] = useState("");
+    const [toggle, setToggle] = useState(false);
 
     const navLinks = ["Home", "About", "Work", "Contact"];
 
@@ -24,7 +25,8 @@ const NavBar = () => {
                     <img src="./nav_img1.png" alt="logo" className='w-10 h-10 object-contain' />
                 </Link>
                 <div className='grid'>
-                    <div className='flex items-center justify-between gap-8'>
+                    {/* Desktop view */}
+                    <div className='hidden md:flex lg:flex xl:flex items-center justify-between gap-8'>
                         {navLinks.map((text) => (
                             <div
                                 key={text}
@@ -50,11 +52,31 @@ const NavBar = () => {
                         ))}
                     </div>
                 </div>
-                <div className='grid'>
+                <div className='lg:grid md:grid hidden'>
                     <div className='flex items-center gap-3'>
                         <img src="./facebook_ic.png" className='w-6 h-6 object-contain' alt="" />
                         <img src="./github_ic.png" className='w-6 h-6 object-contain' alt="" />
                         <img src="./twitterX_ic.png" className='w-6 h-6 object-contain' alt="" />
+                    </div>
+                </div>
+
+                {/* mobile view */}
+                <div className='md:hidden lg:hidden xl:hidden flex flex-1 justify-end items-center'>
+                    <img src={toggle ? "./menu_icon.png" : "./menu_close_icon.png"} alt='menu' className='w-7 h-7 object-contain cursor-pointer'
+                        onClick={() => setToggle(!toggle)}
+                    />
+                    <div className={`${!toggle ? 'hidden' : 'flex'} p-6 bg-secondary absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                        <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                            {navLinks.map((text) => (
+                                <li
+                                    key={text}
+                                    className={`cursor-pointer px-3 py-2 rounded-md ${active === text ? "text-[#F0AEFF] font-bold bg-[rgba(240,174,255,0.08)]" : "text-white"}`}
+                                    onClick={() => setActive(text)}
+                                >
+                                    <span className={`${styles.NavLinkText}`}>{text}</span>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
